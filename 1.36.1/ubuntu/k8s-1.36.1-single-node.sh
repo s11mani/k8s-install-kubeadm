21fb87f9a9719c9ab4978d313f6105c4afe60c4f
Manideep <88645307+s11mani@users.noreply.github.com>
2026-05-30 10:58:59 +0530

    Update k8s-1.36.1-single-node.sh

--- 1.36.1/ubuntu/k8s-1.36.1-single-node.sh
+++ 1.36.1/ubuntu/k8s-1.36.1-single-node.sh
@@ -140,3 +140,11 @@ echo ""
 echo "===== System Pods ====="
 kubectl get pods -A
 echo ""
+
+echo ""
+echo "Installing localstorage class Rancher"
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
+kubectl patch storageclass local-path \
+-p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+echo ""
